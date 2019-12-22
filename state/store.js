8ae7eb02e0b447684acee40ea71e5d752323a53e
Lukas Ružauskas <nequadora@gmail.com>
2019-12-22 14:35:14 +0200

    fixed and updated dependencies

--- state/store.js
+++ state/store.js
@@ -1,6 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger'
 import reducers from './reducers';
 import questions from '../data/questions';
-
-export default createStore(reducers, { questions }, applyMiddleware(logger));
+import { composeWithDevTools } from 'redux-devtools-extension';
+// Neveikia tas composeWithDevTools, not sure why.
+// TO DO: fix;
+export default createStore(reducers, { questions }, composeWithDevTools(
+    applyMiddleware(logger)
+));
